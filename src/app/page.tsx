c4a341c5432178a027352643f2c73031652bf737
4nglp <4nglpw@gmail.com>
2025-07-14 00:26:30 +0100

    vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import Main from "@/components/Main";
 import ExperienceEducationSection from "@/components/Experience&Education";
 import TechStack from "@/components/TechStack";
@@ -5,6 +6,7 @@ import TechStack from "@/components/TechStack";
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Main />
       <ExperienceEducationSection />
       <TechStack />
